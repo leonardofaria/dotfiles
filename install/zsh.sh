604c937fdceae28ccfa7f00289c83d45f553211a
Leonardo Faria <leonardofaria@gmail.com>
2018-05-20 19:49:46 -0700

    Add plugins

--- install/zsh.sh
+++ install/zsh.sh
@@ -3,3 +3,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 echo 'ZSH_THEME="powerlevel9k/powerlevel9k"' >> ~/.zshrc
+
+echo 'plugins=(git rails ruby npm osx)' >> ~/.zshrc
+
+echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc

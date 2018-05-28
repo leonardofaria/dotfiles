59a367b55555a30bcd1050230cbef7d5a5f97faa
Leonardo Faria <leonardofaria@gmail.com>
2018-05-27 19:04:16 -0700

    Update Docker icons

--- macos/dock.sh
+++ macos/dock.sh
@@ -8,8 +8,9 @@ dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Sourcetree.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/TablePlus.app"
+dockutil --no-restart --add "/System/Library/CoreServices/Applications/Screen Sharing.app"
 
-dockutil --no-restart --add '~/Downloads' --view fan --display folder --allhomes
-dockutil --no-restart --add '/Applications' --view grid --display folder --allhomes
+dockutil --no-restart --add '~/Downloads' --view fan --display folder --sort name --allhomes
+dockutil --no-restart --add '/Applications' --view grid --display folder --sort name --allhomes
 
 killall Dock

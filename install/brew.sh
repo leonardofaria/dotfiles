752f1474c992b26908eca2d8b839e00b67c7c72c
Leonardo Faria <leonardofaria@gmail.com>
2018-12-30 17:31:37 -0800

    Remove brew multi users setup

--- install/brew.sh
+++ install/brew.sh
@@ -5,17 +5,23 @@ fi
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-sudo chgrp -R brew /usr/local/Caskroom
-sudo chmod -R g+w /usr/local/Caskroom
+# Optional: in order to install brew for multiple users, create a group called brew.
+# One way to create a group brew is to open the System Preferences and select Users & Groups.
+# Unlock this screen and press the lower left +. Select the account type Group and name it brew.
+# After creating this group add the users you want to share the brew apps
+# Source: https://medium.com/@leifhanack/homebrew-multi-user-setup-e10cb5849d59
 
-sudo chgrp -R brew /usr/local/Cellar
-sudo chmod -R g+w /usr/local/Cellar
-
-sudo chgrp -R brew /usr/local/Homebrew
-sudo chmod -R g+w /usr/local/Homebrew
-
-sudo chgrp -R brew /Library/Caches/Homebrew
-sudo chmod -R g+w /Library/Caches/Homebrew
+# sudo chgrp -R brew /usr/local/Caskroom
+# sudo chmod -R g+w /usr/local/Caskroom
+#
+# sudo chgrp -R brew /usr/local/Cellar
+# sudo chmod -R g+w /usr/local/Cellar
+#
+# sudo chgrp -R brew /usr/local/Homebrew
+# sudo chmod -R g+w /usr/local/Homebrew
+#
+# sudo chgrp -R brew /Library/Caches/Homebrew
+# sudo chmod -R g+w /Library/Caches/Homebrew
 
 brew update
 brew upgrade

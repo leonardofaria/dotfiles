838e97f63584dca0f8648df11e52e059df7d559b
Leonardo Faria <leonardofaria@gmail.com>
2019-01-24 21:18:32 -0800

    Update shell prompt

--- install/zsh.sh
+++ install/zsh.sh
@@ -8,8 +8,8 @@ echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
 echo 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)' >> ~/.zshrc
 echo 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(root_indicator time)' >> ~/.zshrc
 echo 'POWERLEVEL9K_PROMPT_ON_NEWLINE=true' >> ~/.zshrc
-echo 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="↱"' >> ~/.zshrc
-echo 'POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="↳ "' >> ~/.zshrc
+echo 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%F{blue}\u256D\u2500%f"' >> ~/.zshrc
+echo 'POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{blue}\u2570\uf460%f "' >> ~/.zshrc
 
 echo 'plugins=(git rails ruby npm osx)' >> ~/.zshrc
 

20a82da9326aa6fdcb4a87cbe1d442179661aa99
Leonardo Faria <leonardofaria@gmail.com>
2020-02-10 19:46:44 -0800

    Move setup to repository

--- install/zsh.sh
+++ install/zsh.sh
@@ -4,15 +4,6 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo 'ZSH_THEME="powerlevel9k/powerlevel9k"' >> ~/.zshrc
-
-echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
-echo 'POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)' >> ~/.zshrc
-echo 'POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(root_indicator time)' >> ~/.zshrc
-echo 'POWERLEVEL9K_PROMPT_ON_NEWLINE=true' >> ~/.zshrc
-echo 'POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%F{blue}\u256D\u2500%f"' >> ~/.zshrc
-echo 'POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{blue}\u2570\uf460%f "' >> ~/.zshrc
-
-echo 'plugins=(git rails ruby npm osx zsh-autosuggestions)' >> ~/.zshrc
-
 echo 'source $ZSH/oh-my-zsh.sh' >> ~/.zshrc
+
+echo 'source ~/.dotfiles/runcom/.zshrc' >> ~/.zshrc
---
+++ runcom/.zshrc
@@ -0,0 +1,15 @@
+ZSH_THEME="powerlevel9k/powerlevel9k"
+POWERLEVEL9K_MODE='nerdfont-complete'
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir vcs)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(nvm)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%F{blue}\u256D\u2500%f"
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%F{blue}\u2570\uf460%f "
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+POWERLEVEL9K_SHORTEN_STRATEGY="none"
+POWERLEVEL9K_VCS_GIT_HOOKS=(vcs-detect-changes git-untracked git-aheadbehind git-remotebranch)
+
+plugins=(git rails ruby npm osx zsh-autosuggestions nvm brew)
+
+source $ZSH/oh-my-zsh.sh

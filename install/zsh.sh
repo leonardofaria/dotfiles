93214abcdbd935a3a7dccc34f8b2b21439ef9de4
Leonardo Faria <leonardofaria@gmail.com>
2021-07-09 09:43:14 -0700

    Add Powerlevel10k

--- install/zsh.sh
+++ install/zsh.sh
@@ -1,6 +1,6 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 

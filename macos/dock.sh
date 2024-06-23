314cde05db0a3730c93521ad17711d59486d85b8
Leonardo Faria <leonardofaria@gmail.com>
2024-06-23 14:30:46 -0700

    Update configs after new computer install

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Arc.app"
+dockutil --no-restart --add "/Applications/Google Chrome Canary.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/TablePlus.app"
-dockutil --no-restart --add "/System/Library/CoreServices/Applications/Screen Sharing.app"
+dockutil --no-restart --add "/Applications/SourceTree.app"
+dockutil --no-restart --add "/Applications/Postico 2.app"
+dockutil --no-restart --add "/Applications/Warp.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/Notion.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/Reminders.app"
+dockutil --no-restart --add "/Applications/Calendar.app"
+dockutil --no-restart --add "/Applications/System Settings.app"
+dockutil --no-restart --add "/Applications/1Password.app"
+dockutil --no-restart --add "/Applications/Utilities/Screen Sharing.app"
 
 dockutil --no-restart --add '~/Downloads' --view fan --display folder --sort name --allhomes
 dockutil --no-restart --add '/Applications' --view grid --display folder --sort name --allhomes

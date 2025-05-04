4ff9afa83c41c5c418134621daa5acc8c04208d8
Leonardo Faria <leonardofaria@gmail.com>
2025-05-03 18:59:32 -0700

    Add new aliases

--- install/npm.sh
+++ install/npm.sh
@@ -1,26 +1,32 @@
-if ! is-executable brew -o ! is-executable git; then
-  echo "Skipped: npm (missing: brew and/or git)"
-  return
-fi
+# if ! is-executable brew -o ! is-executable git; then
+#   echo "Skipped: npm (missing: brew and/or git)"
+#   return
+# fi
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+# if ! is-executable nvm; then
+#   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+#   export DOTFILES_BREW_PREFIX_NVM=`brew --prefix nvm`
+#   set-config "DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_CACHE"
+#   . "${DOTFILES_DIR}/system/.nvm"
+#   nvm install stable
+#   nvm alias default stable
+#   return
+# fi
 
-export DOTFILES_BREW_PREFIX_NVM=`brew --prefix nvm`
-set-config "DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_BREW_PREFIX_NVM" "$DOTFILES_CACHE"
+# # install bun as well
+# if ! is-executable bun; then
+#   curl -fsSL https://bun.sh/install | bash
+#   return
+# fi
 
-. "${DOTFILES_DIR}/system/.nvm"
-nvm install stable
-nvm alias default stable
+# # and pnpm
+# if ! is-executable pnpm; then
+#   curl -fsSL https://get.pnpm.io/install.sh | sh -
+#   return
+# fi
 
-# install bun as well
-curl -fsSL https://bun.sh/install | bash
-
-# and pnpm
-curl -fsSL https://get.pnpm.io/install.sh | sh -
-
-# Globally install with npm
+# Globally install with pnpm
 packages=(
-  npm
   dockly
   fast-cli
   ffmpeg-progressbar-cli
